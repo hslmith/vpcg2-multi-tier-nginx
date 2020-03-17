595271d0b03ff265d85f0ab93f5be9ced25d98e4
hslmith <47323378+hslmith@users.noreply.github.com>
2020-03-17 14:10:22 -0500

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,19 +1,45 @@
+variable "ibmcloud_region" {
+  description = "Preferred IBM Cloud region to use for your infrastructure"
+  default = "us-south"
+}
+
+
+variable "vpc_name" {
+  default = "vpc-demo003"
+  description = "Name of your VPC"
+}
+
 variable "zone1" {
   default = "us-south-1"
+  description = "Define the 1st zone of the region"
 }
 
 variable "zone2" {
   default = "us-south-2"
+  description = "Define the 2nd zone of the region"
 }
 
-variable "ssh_public_key" {
-  default = "~/.ssh/id_rsa.pub"
+variable "zone1_cidr" {
+  default = "172.16.1.0/24"
+  description = "CIDR block to be used for zone 1"
+}
+
+variable "zone2_cidr" {
+  default = "172.16.2.0/24"
+  description = "CIDR block to be used for zone 2"
+}
+
+variable "ssh_key_name" {
+  default = ""
+  description = "Name of existing VPC SSH Key"
 }
 
 variable "image" {
-  default = "7eb4e35b-4257-56f8-d7da-326d85452591"
+  default = "r006-14140f94-fcc4-11e9-96e7-a72723715315"
+  description = "OS Image ID to be used for virtual instances"
 }
 
 variable "profile" {
-  default = "b-2x8"
+  default = "cx2-2x4"
+  description = "Instance profile to be used for virtual instances"
 }
